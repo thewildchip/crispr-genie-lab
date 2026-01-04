00fecc316e469987192d4b38606dfcf11ec78ed3
Daniel Panoor <fn.danpan@gmail.com>
2026-01-04 17:10:28 +0100

    Added export_as_csv_and_pkl() to utils

--- src/mypackage/utils.py
+++ src/mypackage/utils.py
@@ -1,6 +1,12 @@
+import pandas as pd
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 def print_validation(val_y, pred_y):
     print("MAE: ", mean_absolute_error(val_y, pred_y))
     print("MSE: ", mean_squared_error(val_y, pred_y))
     print("R2:", r2_score(val_y, pred_y))
+
+
+def export_as_csv_and_pkl(df: pd.DataFrame, file_path_without_extension: str): 
+    df.to_csv(f"{file_path_without_extension}.csv", index=False)
+    df.to_pickle(f"{file_path_without_extension}.pkl")
